0562a370f8f01b0aeec484d3da852e31d8bf8ad0
eHolmesSchool <squippysquerp@gmail.com>
2023-07-10 16:19:11 -0600

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Wawaweewa");
+        Debug.Log("If you see this Text Me AAAAAAAAAA");
     }
 
     // Update is called once per frame
