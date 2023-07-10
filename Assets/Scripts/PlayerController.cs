0f57cd75efe28b8290b21c6b871f4a2cad2b7abc
eHolmesSchool <squippysquerp@gmail.com>
2023-07-10 16:15:12 -0600

    Attached script to Player and added Hello World

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,12 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Wawaweewa");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
