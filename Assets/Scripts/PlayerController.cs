16d88bc28d0176ed0bc3384d692c79ed6c1706ba
eHolmesSchool <squippysquerp@gmail.com>
2023-07-12 17:59:01 -0600

    Coded plus adjusted the mousse a bit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,31 +6,91 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public PlayerState currentPlayerState = PlayerState.None;
+    [SerializeField] GameObject Player;
+    Rigidbody2D rbPlayer;
+
+    public enum PlayerState
+    {
+        None,
+        Alive,
+        Dashing,
+        Reloading,
+        Dead,
+
+    }
 
 
     void Start()
     {
         Debug.Log("If you see this Text Evan AAAAAAAAAA");
+        rbPlayer = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        switch (currentPlayerState)
+
+
+        switch (currentPlayerState) //Starts on None
         {
+            case PlayerState.None: //Moves to Alive
+
+                currentPlayerState = PlayerState.Alive;
+                break;
+
+
+            case PlayerState.Alive: //Can become Dashing or Reloading or Dead
+
+                PlayerMovement();
+                PointToMouse();
+                
+
+
+                break;
+
+
+            case PlayerState.Dashing: //Can become Alive or Dead(?) 
+
+
+                break;
+
+
+            case PlayerState.Reloading: //Can become Alive or Dead
+
+
+                break;
+
+
+            case PlayerState.Dead: //Can become None
+
+
+                break;
 
         }
 
+
+
     }
 
 
+    private void PlayerMovement()
+    {
+        rbPlayer.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-    public enum PlayerState{
-        None,
-        Alive,
-        Dashing,
-        //Shooting, Unless we NEED it I dont think this one was a good idea. Cause we can shoot
-        Reloading,
-        Dead,
 
+        if ( Input.GetAxis("Horizontal") < -0.05 || Input.GetAxis("Horizontal") > 0.05)
+        {
+            if (Input.GetAxis("Vertical") < -0.05 || Input.GetAxis("Vertical") > 0.05)
+            {
+
+            }
+        }
     }
+
+
+    private void PointToMouse()
+    {
+        return;
+    }
+
+
 }
